c9833b2bb078581997ea4ced401817cd2d5cb6e4
Naheed <naheed.official99@gmail.com>
2026-07-16 11:49:33 +0500

    Fix ResumePreview: render sections in sectionOrder, hide skipped sections

--- src/components/ResumePreview/ResumePreview.jsx
+++ src/components/ResumePreview/ResumePreview.jsx
@@ -8,19 +8,29 @@ import PersonalInfoPreview from "./PersonalInfoPreview";
 import ProjectsPreview from "./ProjectsPreview";
 import SkillsPreview from "./SkillsPreview";
 
+const PREVIEW_COMPONENTS = {
+  education: EducationPreview,
+  experience: ExperiencePreview,
+  projects: ProjectsPreview,
+  certifications: CertificationsPreview,
+  languages: LanguagesPreview,
+  skills: SkillsPreview,
+  interests: InterestsPreview,
+};
+
 function ResumePreview() {
   const { resumeData } = useResume();
 
   return (
     <div className="bg-white dark:bg-[#233256] rounded-lg shadow-lg p-8 sm:p-10 max-w-2xl mx-auto text-[#1C2541] dark:text-[#F2EFE9]">
       <PersonalInfoPreview personalInfo={resumeData.personalInfo} />
-      <EducationPreview education={resumeData.education} />
-      <ExperiencePreview experience={resumeData.experience} />
-      <ProjectsPreview projects={resumeData.projects} />
-      <CertificationsPreview certifications={resumeData.certifications} />
-      <LanguagesPreview languages={resumeData.languages} />
-      <SkillsPreview skills={resumeData.skills} />
-      <InterestsPreview interests={resumeData.interests} />
+
+      {resumeData.sectionOrder
+        .filter((key) => !resumeData.skippedSections.includes(key))
+        .map((key) => {
+          const Component = PREVIEW_COMPONENTS[key];
+          return <Component key={key} {...{ [key]: resumeData[key] }} />;
+        })}
     </div>
   );
 }
